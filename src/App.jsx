5a6bc62b49c8329519ea62bf4fa7cd51f9c82876
YoungJ <wns4011@daum.net>
2023-11-10 15:38:18 +0900

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+function App() {
+  return <div>
+      <Header></Header>
+
+
+
+      <Footer></Footer>
+  </div>;
+}
+
+export default App;
